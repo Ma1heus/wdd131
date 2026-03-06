20440555e76f0a50de9d42199c63a5e03c27f82f
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-06 18:08:20 -0300

    script added

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,42 +1,8 @@
-// select the DOM elements for output
-const full = document.querySelector("#full");
-const worldfull = document.querySelector("#world-full");
-const short = document.querySelector("#short");
-const medium = document.querySelector("#medium");
-const year = document.querySelector("#year");
-const month = document.querySelector("#month");
-const day = document.querySelector("#day");
-const dayofweek = document.querySelector("#dayofweek");
+// Get current year
+const currentYear = new Date().getFullYear();
 
-// use the date object
-const today = new Date();
+// Insert year in footer
+document.getElementById("currentyear").textContent = currentYear;
 
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "full"
-    }
-).format(today)}</span>`;
-worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-UK",
-    {
-        dateStyle: "full"
-    }
-).format(today)}</span>`;
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "short"
-    }
-).format(today)}</span>`;
-medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "medium"
-    }
-).format(today)}</span>`;
-
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
-day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+// Get last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
