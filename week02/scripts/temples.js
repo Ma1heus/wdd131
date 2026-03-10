e0bf016913ea36f7d030aaea95800136047bd6fe
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-10 12:15:15 -0300

    Finishing the Header for mobile devices

---
+++ week02/scripts/temples.js
@@ -0,0 +1,18 @@
+// Select the ID "menu-button"
+const button = document.querySelector("#menu-button");
+//Select the ID "menu"
+const menu = document.querySelector("#menu");
+
+// Add an Event to the button when it's clicked
+button.addEventListener("click", function(){
+    menu.classList.toggle("open");
+})
+
+// Get current year
+const currentYear = new Date().getFullYear();
+
+// Insert year in footer
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
