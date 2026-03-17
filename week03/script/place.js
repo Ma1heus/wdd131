211c4029bccf8f7c1d626685353add0dad19bdc5
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-17 19:17:13 -0300

    In this first commit I just create all the neces- saries files and HTML to proceed without any concern
    I also introduced some information in the HTML and
    created a header, main, and footer.

---
+++ week03/script/place.js
@@ -0,0 +1,9 @@
+// Get current year
+const currentYear = new Date().getFullYear();
+
+// Insert year in footer
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
