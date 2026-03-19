b2b1c3666055056e3b96a9aa6c1287dba771bb22
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-19 15:07:24 -0300

    I finished to mobile style, and also added all the necessary images to the project. I also completed the JS code for this project.
    I still need to complete the Web style, so all of the project will be done.

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -7,3 +7,26 @@ document.getElementById("currentyear").textContent = currentYear;
 // Get last modified date
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
 
+const temperature = 10;
+const windSpeed = 5;
+
+document.getElementById("temperature").textContent = temperature + "°C";
+document.getElementById("wind").textContent = windSpeed + " km/h";
+document.getElementById("conditions").textContent = "Partly Cloudy";
+
+function calculateWindChill(temp, wind) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(wind, 0.16) +
+        0.3965 * temp * Math.pow(wind, 0.16)
+    ).toFixed(1);
+}
+
+let windChill = "N/A";
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = calculateWindChill(temperature, windSpeed) + "°C";
+}
+
+document.getElementById("wind-chill").textContent = windChill;
